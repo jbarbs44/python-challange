c270e783bd632064746ac67c0e544ce6eb7097e0
Jason Barbagallo <jasonbarbagallo@gmail.com>
2023-03-13 21:15:47 -0400

    Final PyPoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -15,37 +15,43 @@ with open(election_data_csv) as csv_file:
     Complete_Candidates= []
     Candidates = []
     Votes_For_Candidates= {}
+    TotalVotes= 0 
 
     for row in csv_reader:
         Total_Votes.append(row[0])
         Vote_Total =len(Total_Votes)
         Complete_Candidates.append(row[2])
-    for Candidate in Complete_Candidates:
-        Candidates =[]
-        if Candidate not in Candidates:
-            Candidates.append(Candidate)
-            print(Candidate)
+        TotalVotes +=1
+        Candidates = row[2]
+        if Candidates in Votes_For_Candidates:
+            Votes_For_Candidates[Candidates] += 1
+        else:
+            Votes_For_Candidates[Candidates] = 1
 
+#Percent Vote for Each Candidate
+#Percent for Charles Casper Stockham
+Votes_For_Candidates["Charles Casper Stockham%"] = round((Votes_For_Candidates["Charles Casper Stockham"]/TotalVotes) * 100,3)
+print(Votes_For_Candidates["Charles Casper Stockham%"])
 
+#Percent for Diana DeGette
+Votes_For_Candidates["Diana DeGette%"] = round((Votes_For_Candidates["Diana DeGette"]/TotalVotes) * 100,3)
+print(Votes_For_Candidates["Diana DeGette%"])
 
+#Percent for Raymon Anthony Doane
+Votes_For_Candidates["Raymon Anthony Doane%"] = round((Votes_For_Candidates["Raymon Anthony Doane"]/TotalVotes) * 100,3)
+print(Votes_For_Candidates["Raymon Anthony Doane%"])
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+#To find the candidate winner
+Votes_For_Candidates_Winner = max(Votes_For_Candidates, key=Votes_For_Candidates.get)
 
 
 print("Election Results")
 print("----------------------------------------")
 print(f"Total Votes: {Vote_Total}")
 print("----------------------------------------")
-    
+print(f"Charles Casper Stockham:  {Votes_For_Candidates['Charles Casper Stockham%']}% ({Votes_For_Candidates['Charles Casper Stockham']})")
+print(f"Diana DeGette:  {Votes_For_Candidates['Diana DeGette%']}% ({Votes_For_Candidates['Diana DeGette']})")
+print(f"Raymon Anthony Doane:  {Votes_For_Candidates['Raymon Anthony Doane%']}% ({Votes_For_Candidates['Raymon Anthony Doane']})")
+print("----------------------------------------")
+print(f"Winner: {Votes_For_Candidates_Winner}")
+print("----------------------------------------")
